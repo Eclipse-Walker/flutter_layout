5dfaa9d64f1dc642adc73e8f719bb2920d30ad09
EclipseWalker <phisanuratv100@hotmail.com>
2024-11-15 15:54:39 +0700

    reface: format code

--- lib/responsive/desktop_body.dart
+++ lib/responsive/desktop_body.dart
@@ -1,14 +1,14 @@
 import 'package:flutter/material.dart';
 
 class DesktopBody extends StatelessWidget {
-  const DesktopBody({Key? key}) : super(key: key);
+  const DesktopBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[200],
       appBar: AppBar(
-        title: Text('Desktop'),
+        title: const Text('Desktop'),
       ),
       body: Padding(
         padding: const EdgeInsets.all(8.0),
@@ -20,7 +20,6 @@ class DesktopBody extends StatelessWidget {
                 child: Column(
                   children: [
                     Padding(
-                      // video
                       padding: const EdgeInsets.all(8.0),
                       child: AspectRatio(
                         aspectRatio: 16 / 9,
@@ -30,7 +29,6 @@ class DesktopBody extends StatelessWidget {
                         ),
                       ),
                     ),
-                    // comment
                     Container(
                       height: 500,
                       child: ListView.builder(
--- lib/responsive/mobile_body.dart
+++ lib/responsive/mobile_body.dart
@@ -1,19 +1,18 @@
 import 'package:flutter/material.dart';
 
 class MobileBody extends StatelessWidget {
-  const MobileBody({Key? key}) : super(key: key);
+  const MobileBody({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.deepPurple[200],
       appBar: AppBar(
-        title: Text('Mobile'),
+        title: const Text('Mobile'),
       ),
       body: Column(
         children: [
           Padding(
-            // video
             padding: const EdgeInsets.all(8.0),
             child: AspectRatio(
               aspectRatio: 16 / 9,
@@ -23,7 +22,6 @@ class MobileBody extends StatelessWidget {
               ),
             ),
           ),
-          // comment
           Expanded(
             child: ListView.builder(
                 itemCount: 8,
