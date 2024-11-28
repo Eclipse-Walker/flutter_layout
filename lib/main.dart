4b94f1f30e9ebfc953234cba4cf25dce6002415f
EclipseWalker <phisanuratv100@hotmail.com>
2024-11-28 13:52:24 +0700

    feat: custom font

--- lib/main.dart
+++ lib/main.dart
@@ -11,18 +11,43 @@ void main() {
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
+  static const _prodution = false;
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
       theme: ThemeData(
-        textTheme: GoogleFonts.dynaPuffTextTheme(
-          Theme.of(context).textTheme,
+        useMaterial3: true,
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: Colors.purple,
+          brightness: Brightness.dark,
         ),
-        fontFamily: 'Poppins',
+        textTheme: TextTheme(
+          displayLarge: const TextStyle(
+            fontSize: 72,
+            fontWeight: FontWeight.bold,
+          ),
+          titleLarge: GoogleFonts.dynaPuff(
+            fontSize: 30,
+            fontStyle: FontStyle.normal,
+          ),
+          bodyMedium: GoogleFonts.dynaPuff(),
+          displaySmall: GoogleFonts.dynaPuff(),
+          titleSmall: GoogleFonts.dynaPuff(),
+          bodyLarge: GoogleFonts.dynaPuff(),
+          bodySmall: GoogleFonts.dynaPuff(),
+          displayMedium: GoogleFonts.dynaPuff(),
+          headlineLarge: GoogleFonts.dynaPuff(),
+          headlineMedium: GoogleFonts.dynaPuff(),
+          headlineSmall: GoogleFonts.dynaPuff(),
+          labelLarge: GoogleFonts.dynaPuff(),
+          labelMedium: GoogleFonts.dynaPuff(),
+          labelSmall: GoogleFonts.dynaPuff(),
+          titleMedium: GoogleFonts.dynaPuff(),
+        ),
       ),
-      // home: const MainMenuPage(), //main page
-      home: const PairPricePage(), //dev page
+      home: _prodution ? const MainMenuPage() : const PairPricePage(),
     );
   }
 }
